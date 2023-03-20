0e701bf81f45e2bfca6ff2a70e2a3817ccb59dd0
mutaliev06 <maga_mutaliev06@mail.ru>
2023-03-20 09:54:34 +0300

    отображение описания

--- src/app/components/description-modal/description-modal.component.ts
+++ src/app/components/description-modal/description-modal.component.ts
@@ -1,17 +1,26 @@
 import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {housing, ICardClass} from "../card/card.component";
 
 @Component({
   selector: 'app-description-modal',
   templateUrl: './description-modal.component.html',
-  styleUrls: ['./description-modal.component.css']
+  styleUrls: ['./description-modal.component.css'],
+
 })
 export class DescriptionModalComponent implements OnInit {
+  housing: ICardClass[];
 
-
-  constructor() {
+  constructor(private route: ActivatedRoute) {
+    this.housing = [];
   }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+
+    if (id) {
+      this.housing = housing.filter(x => x.id === Number(id))
+    }
   }
 
 }
